566fd39cf17270db15643be3262e9a742e989f51
edwardszczepanski <eszczepa@uoregon.edu>
2016-09-17 10:07:39 -0400

    More generic metrics

--- testing.js
+++ testing.js
@@ -8,11 +8,25 @@ class Service {
   }
 }
 
+// Backend Categories
 var Python = new Service(7, 8, 1, 8, 3);
 var Ruby = new Service(7, 8, 1, 8, 3);
 var Node = new Service(7, 8, 1, 8, 3);
 var Java = new Service(7, 8, 1, 8, 3);
 var CSharp = new Service(7, 8, 1, 8, 3);
 
-console.log(Python.trendiness);
+// Front end framework, DB, backend language, 
+var React = new Service(7, 8, 1, 8, 3);
+var Angular = new Service(7, 8, 1, 8, 3);
+var jQuery = new Service(7, 8, 1, 8, 3);
+var Backbone = new Service(7, 8, 1, 8, 3);
+var Ember = new Service(7, 8, 1, 8, 3);
+
+// Database
+var MongoDB = new Service(7, 8, 1, 8, 3);
+var PostgreSQL = new Service(7, 8, 1, 8, 3);
+var MySQL = new Service(7, 8, 1, 8, 3);
+var CockroachDB = new Service(7, 8, 1, 8, 3);
+var Cassandra = new Service(7, 8, 1, 8, 3);
+
 
