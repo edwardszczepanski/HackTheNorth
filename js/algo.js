78b0959aa4fd60625ab8409c0ea272cd5bcc2415
edwardszczepanski <eszczepa@uoregon.edu>
2016-09-17 19:35:03 -0400

    Fixed some bugs

--- js/algo.js
+++ js/algo.js
@@ -20,8 +20,8 @@ class Importance {
 }
 
 var cityData = [
-new City("Toronto, Ontario", 1, 1, 1, 1, 1, 1),
-new City("Montreal, Quebec", 1, 1, 1, 1, 1, 1),
+new City("Toronto, Ontario", 1, 2, 3, 4, 5, 1),
+new City("Montreal, Quebec", 100, 100, 100, 100, 100, 1),
 new City("Vancouver, British Columbia", 1, 1, 1, 1, 1, 1),
 new City("Ottawa, Ontario", 1, 1, 1, 1, 1, 1),
 new City("Calgary, Alberta", 1, 1, 1, 1, 1, 1),
@@ -49,6 +49,14 @@ new City("Kingston, Ontario", 1, 1, 1, 1, 1, 1)
 
 var exampleImportance = new Importance(4,3,4,2,1);
 
+function compare(a, b) {
+    if (a.score > b.score) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
 function create(importance, data){
     for (var i = 0; i < data.length; ++i){
         if (true) {
@@ -59,7 +67,6 @@ function create(importance, data){
             sum += data[i].four  * importance.four;
             sum += data[i].five  * importance.five;
             data[i].score = sum;
-            //console.log(data);
             console.log(data[i].score);
         } else {
             // Yelp Integration will come later
@@ -68,3 +75,6 @@ function create(importance, data){
 }
 
 create(exampleImportance, cityData);
+cityData.sort(compare);
+console.log("arst");
+console.log(cityData);
